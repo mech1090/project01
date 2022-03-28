ded27ca26946e818402eda5b4561c424c7430011
Shahzad <mech1090@gmail.com>
2022-03-27 20:21:59 -0600

    dark mode page

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
+
+let theme = true
+
 console.dir(document)
 const body = document.querySelector('body')
 const h1 = document.createElement('h1')
-h1.textContent = 'hello world'
+h1.textContent = ' Hello world'
 body.appendChild(h1)
+
+
+const btn = document.createElement('button')
+btn.textContent = 'Dark mode'
+body.appendChild(btn)
+
+btn.addEventListener('click', ()=>{
+    if(theme){
+        body.style.backgroundColor='black'
+        h1.style.color='white'
+    }
+    else{
+        body.style.backgroundColor='white'
+        h1.style.color='black'
+    }
+    theme = !theme
+})
